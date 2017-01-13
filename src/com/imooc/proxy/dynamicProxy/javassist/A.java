7b261a4fb0d74796c198faa8ccddd55aa4329448
yangcheng <hnayyc@163.com>
2017-01-13 17:47:05 +0800

    添加基于javassist实现的动态代理源码

---
+++ src/com/imooc/proxy/dynamicProxy/javassist/A.java
@@ -0,0 +1,10 @@
+package com.imooc.proxy.dynamicProxy.javassist;
+
+public class A {
+	public void save(){
+        System.out.println("保存商品");
+    }
+    public void del(){
+        System.out.println("删除商品");
+    }
+}
